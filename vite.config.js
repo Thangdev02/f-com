d9181cdef21e7de3fe66e7f4e6735a9e1cef9156
Thangdev02 <thangdev02@gmail.com>
2025-11-06 13:55:57 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: "/", // cần để "/" nếu deploy root domain
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001", // json-server local
+        target: "http://localhost:3001", // chỉ dùng khi dev local
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-});
+  build: {
+    outDir: "dist",
+  },
+}));
