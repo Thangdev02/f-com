ec82758726f65f2f49f3938c0c762bc116580207
Thangdev02 <thangdev02@gmail.com>
2025-11-06 14:06:07 +0700

    len di

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: "/", // cần để "/" nếu deploy root domain
+  base: "/", 
+  build: { outDir: "dist" },
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3001", // chỉ dùng khi dev local
+        target: "http://localhost:3001", // chỉ dev local
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-  build: {
-    outDir: "dist",
-  },
-}));
+});
